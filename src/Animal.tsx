e5c524f2a42580de14f1c26feca8422c93aff616
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-12-02 20:20:28 -0300

    Add Animal Component Style

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -1,5 +1,20 @@
 import "./Animal.css"
 
-export default function(){
-    return <div className="animal">Animal</div>
+interface AnimalProps{
+    icone: string;
+    nome: string;
+    peso: number;
+    extincao: boolean;
+    cor: string;
+}
+
+export default function({icone, nome, peso, extincao, cor}:AnimalProps){
+    return (
+        <div className="animal" style={{background: cor}}>
+            <div>{icone}</div>
+            <div>{`${nome}`}</div>
+            <div>{`Peso: ${peso}kg`}</div>
+            <div>{`Extinto: ${extincao === true ? "sim" : "não"} `}</div>
+       </div>
+    )
 }
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ function App() {
 
   return (
     <div className='app'>
-      <Animal/>
+      <Animal icone="🐵" nome="Macaco" peso={10} extincao={false} cor="Brown" />
+      <Animal icone="🦁" nome="Leão" peso={50} extincao={false} cor="Yellow"/>
+      <Animal icone="🦄" nome="Unicórnio" peso={70} extincao={true} cor="Purple"/>
+      <Animal icone="🐭" nome="Rato" peso={2} extincao={false} cor="Grey"/>
     </div>
   )
 }
