bf1bda8673a99dc429de32f5d2cd833be4fe02a8
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-12-05 22:13:54 -0300

    Added Exibition Childrens (Animals)

--- src/Exibicao.tsx
+++ src/Exibicao.tsx
@@ -1,17 +1,20 @@
 import './Exibicao.css'
+import { ReactNode } from 'react';
 
 interface ExibicaoProps{
     abertura: Date;
     fechamento: Date;
     cercado:string;
+    children: ReactNode; // tipo genérico, aceita muitos tipos diferentes
 }
 
-export default function Exibicao({abertura: inicio, fechamento: fim, cercado: local}: ExibicaoProps){
+export default function Exibicao({abertura: inicio, fechamento: fim, cercado: local, children:animais}: ExibicaoProps){
     return(
         <div className="Exibicao">
             <div className="inicio">Abertura: {inicio.toLocaleString()}</div>
             <div className="fim">Fechamento: {fim.toLocaleString()}</div>
-            <div className="local">Cercado:     {local}</div>
+            <div className="local">Cercado: {local}</div>
+            <div className='animais'>{animais}</div>
         </div>
     )
 }
